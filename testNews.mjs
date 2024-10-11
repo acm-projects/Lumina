20904cd17204342309f2c542eabd868e851d1ac3
Tanishq_Akasapu <tanishq3511@gmail.com>
2024-10-11 16:11:01 -0500

    update to NewsAPI Lambda and env

--- testNews.mjs
+++ testNews.mjs
@@ -1,31 +1,34 @@
 export const handler = async (event) => {
   
+  let key = process.env.apiKey;
   
-    //const url = 'https://newsapi.org/v2/everything?q=astronomy&sortBy=relevancy&apiKey=5b19dbe069e74920b6f1988f75ac4c95';
-    
-    try {
-      const response = await fetch(geoLocatorURL, {
-        method: 'GET',
-      });
+  const url = new URL('https://newsapi.org/v2/everything?q=astronomy&sortBy=relevancy');
+  url.searchParams.append('apiKey', key);
+
   
-      if (!response.ok) {
-        throw new Error('HTTP error');
-      }
-  
-      const newsData = await response.json();
-      console.log(newsData);
-      return {
-        statusCode:200,
-        body : JSON.stringify(newsData),
-      }
-    }catch (error){
-      console.error('Error:', error);
-      return{
-        statusCode : 500,
-        body : JSON.stringify({ message: "Error making call.", details: error.message }),
-      }
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      throw new Error('HTTP error');
     }
-  };
-  
-  console.log(handler());
+
+    const newsData = await response.json();
+    console.log(newsData);
+    //console.log(response);
+    return {
+      statusCode:200,
+      body : JSON.stringify(newsData.articles[0]), //sends only the first article
+    }
+  }catch (error) {
+    console.error('Error:', error);
+    return{
+      statusCode : 500,
+      body : JSON.stringify({ message: "Error making call.", details: error.message }),
+    }
+  }
+};
+
   
